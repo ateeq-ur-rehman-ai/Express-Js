f0c0285c5c7ca170ea6fd91879bf00d4e6bb04ca
Ateeq Ur Rehman <ateeqkhalid623@gmail.com>
2026-04-10 18:17:59 +0500

    Add files via upload

---
+++ Blog Express/data/blogs.js
@@ -0,0 +1,20 @@
+
+blogs = [
+    {
+        title: "How to get start Javascript",
+        content: "This is a guide to getting started with JavaScript."
+    },
+    {
+        title: "How to get start Python",
+        content: "This is a guide to getting started with Python."
+    },
+    {
+        title: "How to get start Node Js and Express Js",
+        content: "This is a guide to getting started with Node.js and Express.js."
+    },
+    {
+        title: "How to get start MongoDB",
+        content: "This is a guide to getting started with MongoDB."
+    }
+]
+module.exports = blogs;
